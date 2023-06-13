e8fdc964a6e8cb83fd74d6d6b40ed6329e0c78af
kojubivek <kojubivek"gmail.com>
2023-06-14 01:10:53 +1000

    added data to databases

--- src/redux/user/UserAction.js
+++ src/redux/user/UserAction.js
@@ -1,11 +1,11 @@
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 import {
   editUserInfo,
   getUser,
   loginUser,
   postNewUser,
   updatePassword,
-} from "../../helpers/axiosHelper"
+} from "../../helpers/axiosHelper";
 import {
   getUserSuccess,
   loginSuccess,
@@ -13,71 +13,73 @@ import {
   requestFailed,
   requestPending,
   requestSuccess,
-} from "./UserSlice"
+} from "./UserSlice";
 
 export const loginAction = (form) => async (dispatch) => {
   try {
     // set loader
-    dispatch(requestPending())
+    dispatch(requestPending());
 
     // call axios
-    const { status, message, user } = await loginUser(form)
+    const { status, message, user } = await loginUser(form);
 
     status === "success"
       ? dispatch(loginSuccess(user)) && toast[status](message)
-      : dispatch(requestFailed({ status, message })) && toast[status](message)
+      : dispatch(requestFailed({ status, message })) && toast[status](message);
   } catch (error) {
-    dispatch(requestFailed(error))
+    dispatch(requestFailed(error));
   }
-}
+};
 
 export const registerAction = (form) => async (dispatch) => {
   try {
-    dispatch(requestPending())
-    const { status, message } = await postNewUser(form)
+    dispatch(requestPending());
+    const { status, message } = await postNewUser(form);
 
     status === "success"
       ? dispatch(registerSuccess({ status, message })) && toast[status](message)
-      : dispatch(requestFailed({ status, message })) && toast[status](message)
+      : dispatch(requestFailed({ status, message })) && toast[status](message);
   } catch (error) {
-    dispatch(requestFailed(error))
+    dispatch(requestFailed(error));
   }
-}
+};
 
 export const updatePasswordAction = (form) => async (dispatch) => {
   try {
-    dispatch(requestPending())
-    const { status, message } = await updatePassword(form)
+    dispatch(requestPending());
+    const { status, message } = await updatePassword(form);
 
     status === "success"
       ? dispatch(requestSuccess({ status, message })) && toast[status](message)
-      : dispatch(requestFailed({ status, message })) && toast[status](message)
+      : dispatch(requestFailed({ status, message })) && toast[status](message);
   } catch (error) {
-    dispatch(requestFailed(error))
+    dispatch(requestFailed(error));
   }
-}
+};
 
 export const getUserAction = () => async (dispatch) => {
   try {
-    const user = await getUser()
+    const user = await getUser();
     user?._id
       ? dispatch(getUserSuccess(user))
-      : dispatch(requestFailed({ status: "error", message: "User not found!" }))
+      : dispatch(
+          requestFailed({ status: "error", message: "User not found!" })
+        );
   } catch (error) {
-    dispatch(requestFailed(error))
+    dispatch(requestFailed(error));
   }
-}
+};
 
 export const editProfileAction = (form) => async (dispatch) => {
   try {
-    dispatch(requestPending())
+    dispatch(requestPending());
 
-    const user = await editUserInfo(form)
+    const user = await editUserInfo(form);
 
     user
       ? dispatch(requestSuccess(user)) && dispatch(getUserAction())
-      : dispatch(requestFailed())
+      : dispatch(requestFailed());
   } catch (error) {
-    dispatch(requestFailed(error))
+    dispatch(requestFailed(error));
   }
-}
+};
